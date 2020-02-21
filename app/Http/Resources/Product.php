05b54b17c7cad9373235fb1c2c7250fb6952f3b6
Md. Shakil Mahmud <shakilssac@gmail.com>
2020-02-22 04:02:52 +0600

    products added

---
+++ app/Http/Resources/Product.php
@@ -0,0 +1,28 @@
+<?php
+  
+namespace App\Http\Resources;
+   
+use Illuminate\Http\Resources\Json\JsonResource;
+   
+class Product extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'detail' => $this->detail,
+            'stock' => $this->stock,
+            'price' => $this->price,
+            'discount' => $this->discount,
+            // 'created_at' => $this->created_at->format('d/m/Y'),
+            // 'updated_at' => $this->updated_at->format('d/m/Y'),
+        ];
+    }
+}
